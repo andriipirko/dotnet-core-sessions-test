8a38ea80791d88e4c0c5e9d922bac1a4f44bf595
andriipirko <andrii.pirko@gmail.com>
2018-02-28 21:15:45 +0200

    Implement authorization

---
+++ Controllers/AuthorizationController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Session;
+
+namespace WebAPI.Controllers
+{
+    [Produces("application/json")]
+    [Route("api/Authorization")]
+    public class AuthorizationController : Controller
+    {
+        [HttpPost]
+        public void Post(string name = null, string password = null)
+        {
+            if (name == "Bogdan" && password == "KpacaBa")
+            {
+                HttpContext.Session.SetInt32("authorizated", 1);
+            }
+            else
+            {
+                HttpContext.Session.SetInt32("authorizated", 0);
+            }
+        }
+
+        [HttpGet]
+        public string Get()
+        {
+            if (HttpContext.Session.GetInt32("authorizated") == 1)
+            {
+                return "Бодя Яворський чоткий пацан.";
+            }
+            else
+            {
+                return "Спробуй ще разок :)";
+            }
+        }
+    }
+}
