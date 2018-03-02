adbd9e47b3701d5cadd233682c54183c2654ef08
andriipirko <andrii.pirko@gmail.com>
2018-03-02 15:31:38 +0200

    Implement mysql verification

---
+++ Controllers/MysqlController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using WebAPI.Models;
+
+namespace WebAPI.Controllers
+{
+    [Produces("application/json")]
+    [Route("api/Mysql")]
+    public class MysqlController : Controller
+    {
+        [HttpGet]
+        public string Index()
+        {
+            var mysqlController = new MySqlServerInstalled();
+            return mysqlController.IsInstalled() ? "installed" : "not installed";
+        }
+    }
+}
---
+++ Models/MySQLServerInstalled.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ServiceProcess;
+
+namespace WebAPI.Models
+{
+    public class MySqlServerInstalled
+    {
+        public bool IsInstalled()
+        {
+            var result = ServiceController.GetServices();
+            var result_2 = result.Select(m => m.ServiceName).Where(m => m.ToLower().Contains("mysql"));
+            return result_2.ToList().Count != 0;
+        }
+    }
+}
